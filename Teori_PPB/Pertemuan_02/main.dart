12bd06a8820d7e7b739e117c3c323af77806a8ef
edgarnadhif <edgarndf345@gmail.com>
2024-09-26 13:00:00 +0700

    pertemuan 2

---
+++ Teori_PPB/Pertemuan_02/main.dart
@@ -0,0 +1,90 @@
+// void main(){
+//   String namaMahasiswa='aceng'; //deklarasi
+//   double ipk=3.88; //inisialisasi
+
+//   bool isMarried = true; // bolean awalanya biasanya diawali dengan is
+//   bool isMan = true;
+//   bool isStudent = false;
+//   bool isLoading = false;
+
+//   int umur =20;
+//   int jumlahRuangan = 100;
+//   int jumlahSaudaraKandung = 69;
+
+//   double tinggiBadan = 169.0;
+//   double beratBadan = 72.4;
+//   double nilai = 99.9;
+//   double ukuranSepatu = 41.1;
+
+//   String hewanPeliharaan = "kucing";
+//   String namaOrtu = "bambang";
+
+// print(ipk);
+
+// var dynamic
+
+// var nama = "bambang"; // var tidak dapat di ubah,  kecuali tipe datanya sama
+// dynamic alamat = "jalan sudagaran"; // dynamic dapat menyimpan semua macam tipe data
+
+// print(nama);
+// print(alamat);
+
+// nama = "100";
+// alamat =100;
+
+// print(nama);
+// print(alamat);
+
+//}
+
+// void main() {
+//   List<dynamic> daftarMahasiswa = [
+//     "ofa",
+//     false,
+//     2,
+//     3.15,
+//     ["games", "coding"]
+//     ];
+
+//   print(daftarMahasiswa[4][0]);
+
+//   daftarMahasiswa[3]=2.1;
+//   print(daftarMahasiswa[3]);
+
+//   daftarMahasiswa.add("kucing");
+//   print(daftarMahasiswa[5]);
+
+//   print("panjang  data dalam list Mahasiswa : " + daftarMahasiswa.length.toString());
+
+// }
+
+void main() {
+  List<Map<String, dynamic>> fufufafa = [
+    {
+      "nama": "edfar",
+      "umur": 69,
+      "ipk": 4.1,
+      "isMarried": false,
+      "hobi": ["games", "coding"]
+    },
+    {
+      "nama": "ofa",
+      "umur": 69,
+      "ipk": 4.1,
+      "isMarried": true,
+      "hobi": ["games", "coding"]
+    }
+  ];
+
+  print(fufufafa[0]["isMarried"]);
+  print(fufufafa[1]["nama"]);
+  print(fufufafa[1]["hobi"]);
+}
+
+// buatkan algorita
+// a. untuk menghitung rerata dari IPK
+// c. Menjumlah beberapa banyak siswa yang sudah menikah
+
+// 2. Masing" dari point 1.a dan 1.b diimplementasikan dalam kode program
+
+// upload di
