5e0ec204cb9e2dd1bd504c1dbf7fff299c3f223f
edgarnadhif <edgarndf345@gmail.com>
2024-10-01 15:29:09 +0700

    update tugas 3

--- 03_Pengenalan_Dart/Unguided/soal_02/soal2.dart
+++ 03_Pengenalan_Dart/Unguided/soal_02/soal2.dart
@@ -1,6 +1,24 @@
-
 // Buatlah sebuah program dalam Dart yang menampilkan piramida bintang dengan
 // menggunakan for loop. Panjang piramida ditentukan oleh input dari user.
 
+import 'dart:io';
 
+void main() {
+  print("Masukkan panjang piramida:");
+  String? input = stdin.readLineSync();
+  int? panjang = int.tryParse(input!);
 
+  if (panjang != null && panjang > 0) {
+    for (int i = 1; i <= panjang; i++) {
+      for (int j = 0; j < panjang - i; j++) {
+        stdout.write(" ");
+      }
+      for (int k = 0; k < (2 * i - 1); k++) {
+        stdout.write("*");
+      }
+      print("");
+    }
+  } else {
+    print("Input tidak valid, harap masukkan angka yang benar.");
+  }
+}
